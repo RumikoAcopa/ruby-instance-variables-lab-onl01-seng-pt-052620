834ad853c78cf9ace80c97188b2837ee7a65fd2f
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-10 00:29:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,9 @@
-lassie = Dog.new
-lassie.name = "Lassie"
-
-lassie.name #=> "Lassie"
+class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
+end
